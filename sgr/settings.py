1f7605728e643cd7a2ba7c87af42f2e6a4a8a041
Arthur Silva <arxdsilva@gmail.com>
2018-11-12 08:23:05 -0200

    add programs config to INSTALLED_APPS

--- sgr/settings.py
+++ sgr/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'programs.apps.ProgramsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
