41a23c2aa2f5504500482d89812e86108509577b
Arthur Silva <arxdsilva@gmail.com>
2018-11-12 13:23:07 -0200

    add namespace to program resource

--- programs/urls.py
+++ programs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name='program'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:program_id>/', views.detail, name='detail'),
--- sgr/urls.py
+++ sgr/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('programs.urls')),
+    path('', include(('programs.urls', 'programs'), namespace='programs')),
     path('admin/', admin.site.urls),
 ]
