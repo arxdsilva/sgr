96825c24c44e61330e7a590c57bf62d1bc4111f2
Arthur Silva <arxdsilva@gmail.com>
2018-11-12 12:53:05 -0200

    fix tests

--- programs/test_models.py
+++ programs/test_models.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
 from .models import Program
 from datetime import datetime, timedelta
+import pytz
 
 
 class TestProgram(TestCase):
     def setUp(self):
-        now = datetime.now()
-        end = now + datetime.timedelta(minutes = 10)
+        timeZone = datetime.now(pytz.timezone("America/Sao_Paulo"))
+        end = timeZone + timedelta(minutes = 10)
         Program.objects.create(name='Fluminense', ptype=2, end=end)
 
     def test_program_is_live(self):
         f = Program.objects.get(name='Fluminense')
-        self.assertEqual(f.is_live(), True)
+        self.assertEqual(f.is_live, True)
 
     def test_radio_is_globo(self):
         f = Program.objects.get(name='Fluminense')
