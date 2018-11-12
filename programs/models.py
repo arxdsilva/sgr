6905b4943d2443b1e60fde42cf737fce73cca24c
Arthur Silva <arxdsilva@gmail.com>
2018-11-12 09:01:19 -0200

    programs: edit Program so it will have start and end times

--- programs/models.py
+++ programs/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from datetime import datetime    
+
+RADIOS = (
+    (1, _("Radio Globo")),
+    (2, _("CBN")),
+    (3, _("BH"))
+)
 
 # Create your models here.
 class Program(models.Model):
     name = models.CharField(max_length=200)
-    date = models.DateTimeField('date published')
+    radio = models.IntegerField(choices=RADIOS, default=1)
+    start = models.DateTimeField('start date time', default=datetime.now, blank=True)
+    end = models.DateTimeField('end date time', default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
